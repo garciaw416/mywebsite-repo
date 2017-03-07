48fc11b4e4d8fcad437e11503967ea1d5a43a91e
garciaw416 <garciaw416@gmail.com>
2017-03-06 22:15:46 -0500

    website first draft

--- js/main.js
+++ js/main.js
@@ -2,3 +2,36 @@
 //  $('.gallery').addClass('popout').fadeIn(10000);
 //}
 //)
+
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        stagePadding: 20,
+        loop: true,
+        center: true,
+        nav: true,
+        /*navText: ["Previous", "Next"]*/
+        dotsEach: true,
+
+        responsive: {
+            0: {
+                items: 1
+
+            },
+            640: {
+                items: 3
+
+            },
+            768: {
+                items: 3
+            },
+
+            992: {
+                items: 3,
+            },
+
+            1200: {
+                items: 3,
+            }
+        }
+    })
+});
